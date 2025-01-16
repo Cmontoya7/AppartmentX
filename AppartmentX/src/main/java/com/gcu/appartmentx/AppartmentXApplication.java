3fcd71e6d9bdba9fc4bd6c5b41825703a03f9319
Cmontoya7 <chris.l.montoya7@gmail.com>
2025-01-15 19:46:15 -0600

    Add files via upload

---
+++ AppartmentX/src/main/java/com/gcu/appartmentx/AppartmentXApplication.java
@@ -0,0 +1,13 @@
+package com.gcu.appartmentx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppartmentXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppartmentXApplication.class, args);
+    }
+
+}
---
+++ AppartmentX/src/test/java/com/gcu/appartmentx/AppartmentXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcu.appartmentx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppartmentXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
