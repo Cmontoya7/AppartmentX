ed9e6eb3b86f381a2a11bae09ba78184c2e32120
Cmontoya7 <chris.l.montoya7@gmail.com>
2025-01-17 16:32:19 -0600

    Created function hompage and loggin mudules. with non-functioning formatting

---
+++ ApartmentX/src/main/java/com/gcu/controllers/ApartmentXController.java
@@ -0,0 +1,32 @@
+package com.gcu.apartmentx.controllers;
+
+
+import com.gcu.models.LoginModel;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ApartmentXController {
+	
+	@GetMapping("/")
+    public ModelAndView homepage() {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("message", "Test Homepage");
+        mav.setViewName("Homepage");
+        return mav;
+    }
+    
+    @GetMapping("/login")
+    public String display(Model model){
+        model.addAttribute("title", "Login Form");
+        model.addAttribute("loginModel", new LoginModel());
+        return "Login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+    	return "Register";
+    }
+}
