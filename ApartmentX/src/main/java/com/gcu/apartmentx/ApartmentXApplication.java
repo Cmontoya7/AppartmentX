a320530e406b061f64cdeeb2a0e1fffdb0b26c65
Justin Iaconis <JIaconis@my.gcu.edu>
2025-01-17 16:05:20 -0700

    Fix package scanning

--- ApartmentX/src/main/java/com/gcu/apartmentx/ApartmentXApplication.java
+++ ApartmentX/src/main/java/com/gcu/apartmentx/ApartmentXApplication.java
@@ -3,7 +3,7 @@ package com.gcu.apartmentx;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.gcu")
 public class ApartmentXApplication {
 
     public static void main(String[] args) {
