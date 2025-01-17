b2d4619852e1f0016997c08b853e34270bdeb04b
Justin Iaconis <JIaconis@my.gcu.edu>
2025-01-17 16:17:54 -0700

    Combine registration and login/homepage modules

--- ApartmentX/src/main/java/com/gcu/controllers/ApartmentXController.java
+++ ApartmentX/src/main/java/com/gcu/controllers/ApartmentXController.java
@@ -1,10 +1,12 @@
-package com.gcu.apartmentx.controllers;
+package com.gcu.controllers;
 
-
+import com.gcu.models.ApartmentXUser;
 import com.gcu.models.LoginModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -24,13 +26,15 @@ public class ApartmentXController {
         model.addAttribute("loginModel", new LoginModel());
         return "Login";
     }
-
-
-
+    
     @GetMapping("/register")
     public String register() {
     	return "Register";
     }
-
-
+    
+    @PostMapping("/register/submitRegistration") //TEMP will direct to database
+    public String submitRegistrationForm(@ModelAttribute ApartmentXUser user) {
+    	System.out.println("recieved registration: " + user.toString());
+    	return "RegisterSuccess";
+    }
 }
