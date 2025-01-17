486e6bdd928164264cc8545059895e01b90aac10
Justin Iaconis <JIaconis@my.gcu.edu>
2025-01-16 22:09:46 -0700

    Add registration success html page

--- ApartmentX/src/main/java/com/gcu/apartmentx/controllers/ApartmentXController.java
+++ ApartmentX/src/main/java/com/gcu/apartmentx/controllers/ApartmentXController.java
@@ -3,6 +3,9 @@ package com.gcu.apartmentx.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.gcu.apartmentx.ApartmentXUser;
 
 @Controller
 public class ApartmentXController {
@@ -20,5 +23,11 @@ public class ApartmentXController {
     @GetMapping("/register")
     public String register() {
     	return "Register";
+    }
+    
+    @PostMapping("/register/submitRegistration") //TEMP will direct to database
+    public String submitRegistrationForm(@ModelAttribute ApartmentXUser user) {
+    	System.out.println(user.toString());
+    	return "RegisterSuccess";
     }
 }
