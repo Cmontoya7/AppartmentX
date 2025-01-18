092c409a79b94ea432e21ac09a2b27685c648c1e
Cmontoya7 <chris.l.montoya7@gmail.com>
2025-01-18 14:34:50 -0600

    Some Major changes to funtionality, go the navigation of the post loggin to work, along with all links. Waiting on post Registration and need help with nav bar

--- ApartmentX/src/main/java/com/gcu/controllers/ApartmentXController.java
+++ ApartmentX/src/main/java/com/gcu/controllers/ApartmentXController.java
@@ -2,29 +2,50 @@ package com.gcu.apartmentx.controllers;
 
 
 import com.gcu.models.LoginModel;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class ApartmentXController {
-	
-	@GetMapping("/")
-    public ModelAndView homepage() {
-        ModelAndView mav = new ModelAndView();
-        mav.addObject("message", "Test Homepage");
-        mav.setViewName("Homepage");
-        return mav;
+    private final LoginModel loginModel = new LoginModel();
+
+    @GetMapping("/")
+    public String homepage(Model model) {
+        model.addAttribute("title", "Welcome to Apartment X");
+        model.addAttribute("loginModel", loginModel);
+        model.addAttribute("username", loginModel.getUsername());
+        model.addAttribute("loggedIn", loginModel.isLoggedIn());
+        return "Homepage";
     }
-    
+
     @GetMapping("/login")
     public String display(Model model){
-        model.addAttribute("title", "Login Form");
-        model.addAttribute("loginModel", new LoginModel());
+        model.addAttribute("title", "Login");
+        model.addAttribute("loginModel", loginModel);
+        model.addAttribute("username", loginModel.getUsername());
+        model.addAttribute("loggedIn", loginModel.isLoggedIn());
         return "Login";
     }
 
+    @PostMapping("/doLogin")
+    public String doLogin(@Valid LoginModel loginmodel2, BindingResult bindingResult, Model model){
+
+        if (bindingResult.hasErrors()){
+            model.addAttribute("title", "Login Form");
+            return "Login";
+        } else {
+            loginModel.loginTrue();
+            loginModel.setUsername(loginmodel2.getUsername());
+            return "redirect:/";
+        }
+    }
 
 
     @GetMapping("/register")
--- ApartmentX/src/main/java/com/gcu/models/LoginModel.java
+++ ApartmentX/src/main/java/com/gcu/models/LoginModel.java
@@ -7,11 +7,13 @@ public class LoginModel {
 
     @NotNull(message = "User Name is a required field")
     @Size(min = 6, max = 32, message = "User Name must be between 6-32 characters long.")
-    public String username;
+    private String username;
 
     @NotNull(message = "Password is a required field")
     @Size(min = 8, max = 32, message = "User Name must be between 8-32 characters long.")
-    public String password;
+    private String password;
+
+    public boolean loggedIn = false;
 
     public void setUsername(String username) {
         this.username = username;
@@ -27,5 +29,13 @@ public class LoginModel {
 
     public String getPassword() {
         return password;
+    }
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+    public void loginTrue() {
+        this.loggedIn = true;
     }
 }
